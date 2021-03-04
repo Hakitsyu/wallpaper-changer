801ab18ae50f1cd50e8ec465816a9176e464c8d0
vitor <vitorjdev@gmail.com>
2021-03-04 09:59:06 -0300

    Arquivo principal

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace wallpaper_changer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
