c8655105362bded72f9da76f78ca7568d5fae06c
vitor <vitorjdev@gmail.com>
2021-03-04 09:58:43 -0300

    Classe da configuração

---
+++ Config.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace wallpaper_changer
+{
+    class General
+    {
+        public int time { get; set; } = 10000;
+        public bool loop { get; set; } = true;
+    }
+
+    class Config
+    {
+        public General general { get; set; } = null;
+    }
+}
