e48af2967f337d5bbf99a7fc67ba9f3d90f79967
vitor <vitorjdev@gmail.com>
2021-03-04 09:59:48 -0300

    Arquivo geral dos wallpapers

---
+++ Wallpaper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace wallpaper_changer
+{
+    class Wallpaper
+    {
+        public string ImagePath { get; }
+        public string Extension { get; }
+        public string FileName { get; }
+        public string FullFileName { get; }
+
+        public Wallpaper(string imagePath)
+        {
+            ImagePath = imagePath;
+            Extension = Path.GetExtension(imagePath);
+            FileName = Path.GetFileNameWithoutExtension(imagePath);
+            FullFileName = Path.GetFileName(imagePath);
+        }
+    }
+}
