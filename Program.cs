c5f23d90fbf413bbb11b9311dbcf00ac7a48c419
vitor <vitorjdev@gmail.com>
2021-03-05 10:39:34 -0300

    Criação do sistema de debug

--- Program.cs
+++ Program.cs
@@ -6,7 +6,30 @@ namespace wallpaper_changer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Debugger.Debug("Loading application...");
         }
     }
+
+    class Debugger
+    {
+        private static bool Active = true;
+        public enum Type
+        {
+            Info,
+            Error,
+            Success
+        }
+
+        public static void Debug(Type type, string message)
+        {
+            if (Active)
+                Console.WriteLine("[" + Enum.GetName(typeof(Type), type) + "] " + message);
+        }
+
+        public static void Debug(string message)
+        {
+            Debug(Type.Info, message);
+        }
+    }
+
 }
