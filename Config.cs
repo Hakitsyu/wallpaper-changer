b271b510b4eabee6db8ca8ffa61bfb73c638aa3f
vitor <vitorjdev@gmail.com>
2021-03-06 09:14:16 -0300

    Adição do parametro paths e formatação dos paths

--- Config.cs
+++ Config.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace wallpaper_changer
 {
@@ -12,6 +10,20 @@ namespace wallpaper_changer
 
     class Config
     {
-        public General general { get; set; } = null;
+        public General general { get; set; } = new General();
+        public string[] paths { get; set; } = new string[] {};
+
+        public static string[] FormatPaths(string[] paths)
+        {
+            string[] result = new string[paths.Length];
+            for (int i = 0; i < paths.Length; i++)
+            {
+                result[i] = paths[i]
+                    .Replace("{root_path}", Environment.CurrentDirectory)
+                    .Replace("{config_path}", Program.ConfigPath);
+            }
+
+            return result;
+        }
     }
 }
